d34f72c367aa8abbbe996ee19ac5b660fb9ec0e1
Romario <laloromario95@gmail.com>
2022-11-01 15:11:20 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+import './index.css'
+
+function App() {
+  return (
+    <div className="todo-app">
+        <h1>TodoApp</h1>
+        <TodoForm />
+    </div>
+  );
+}
+
+export default App;
